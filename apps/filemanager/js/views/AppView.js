d26ec2a82e02aebde11bb7d69d83114f0cbc7c0a
Med0paW <medopaw@gmail.com>
2013-04-12 13:07:42 +0800

    Change null parameter in CopyTo and MoveTo to empty string.

--- apps/filemanager/js/views/AppView.js
+++ apps/filemanager/js/views/AppView.js
@@ -90,11 +90,11 @@ define(["jquery", "backbone", "utils", "models/AppModel", "models/EntryModel", "
                 console.log(clipboard.action);
                 switch (clipboard.action) {
                     case "Copy": {
-                        clipboard[i].copyTo(parentDirEntry, null, successHandler, Utils.errorHandler);
+                        clipboard[i].copyTo(parentDirEntry, "", successHandler, Utils.errorHandler);
                         break;
                     }
                     case "Move": {
-                        clipboard[i].moveTo(parentDirEntry, null, successHandler, Utils.errorHandler);
+                        clipboard[i].moveTo(parentDirEntry, "", successHandler, Utils.errorHandler);
                         break;
                     }
                     default: {
